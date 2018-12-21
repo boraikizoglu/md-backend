812601b6a161baeef6a9abab597c3f92f16558d9
Bora Ikizoglu <boraikizoglu@gmail.com>
2018-12-22 02:46:01 +0300

    Updated /upload endpoint for jQuery-File-Upload

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -4,6 +4,8 @@ import { check, validationResult } from 'express-validator/check';
 import { QueryResult } from 'pg';
 import * as Correlation from 'node-correlation';
 import * as stats from 'stats-lite';
+import * as csv from 'csvtojson/v2';
+import * as fs from 'fs';
 
 interface IMainController {
     get(): void;
@@ -13,17 +15,18 @@ interface IMainController {
     getStocksList(): void;
 }
 
+// row field is null, it becomes string
 interface IStock {
     Date: String;
     date?: String;
-    Open: Number;
-    High: Number;
-    Low: Number;
-    Close?: Number;
-    close?: Number;
-    'Adj Close': Number;
-    Volume: Number;
-    table_id: Number;
+    Open: Number | String;
+    High: Number | String;
+    Low: Number | String;
+    Close?: Number | String;
+    close?: Number | String;
+    'Adj Close': Number | String;
+    Volume: Number | String;
+    table_id: Number | String;
 }
 
 class MainController implements IMainController {
@@ -34,43 +37,104 @@ class MainController implements IMainController {
         });
     }
 
-    private uploadQueryText(req: Request): string{
+    private uploadQueryText(fileName: string, pathName: string, callback): void {
         let queryText: string = `INSERT INTO "test2" ("stocksymbol",DATE,OPEN,HIGH,LOW,CLOSE, ADJ_CLOSE, VOLUME, TABLE_ID) VALUES`;
-        const data: IStock[] = JSON.parse(req.body.data);
-        const stockSymbol: string = req.body.stock_symbol;
-        const now: number = new Date().getTime();
-        data.forEach((row: IStock, index) => {
-            const rowString: string = index === data.length-1 ? // if it is the last row
-            ` ('${stockSymbol}', '${row.Date}', ${row.Open}, ${row.High}, ${row.Low},${row.Close}, ${row['Adj Close']}, ${row.Volume}, ${now})`
-            :
-            ` ('${stockSymbol}', '${row.Date}', ${row.Open}, ${row.High}, ${row.Low},${row.Close}, ${row['Adj Close']}, ${row.Volume}, ${now}), `;
-
-            queryText += rowString;
+        csv().fromFile(pathName).then((data: IStock[])=>{
+            if(data.length === 0){
+                return callback(true, null);
+            }
+            const stockSymbol: string = fileName;
+            const now: number = new Date().getTime();
+            data.forEach((row: IStock, index) => {
+                // if row does not contain null elemenet, add to query string
+                if(stockSymbol !== 'null' && row.Date !== 'null' &&
+                    row.Open !== 'null' && row.High !== 'null' &&
+                    row.Low !== 'null' && row.Close !== 'null' &&
+                    row['Adj Close'] !== 'null' && row.Volume !== 'null'
+                    ){
+                        const rowString: string = index === data.length-1 ? // if it is the last row
+                        ` ('${stockSymbol}', '${row.Date}', ${row.Open}, ${row.High}, ${row.Low},${row.Close}, ${row['Adj Close']}, ${row.Volume}, ${now})`
+                        :
+                        ` ('${stockSymbol}', '${row.Date}', ${row.Open}, ${row.High}, ${row.Low},${row.Close}, ${row['Adj Close']}, ${row.Volume}, ${now}), `;
+                        queryText += rowString;
+                     }
+            });
+            queryText += 'ON CONFLICT (stockSymbol, DATE, TABLE_ID) DO NOTHING;'; // update if there is a duplicate
+            fs.unlinkSync(pathName); // delete the file
+            callback(null, queryText);
         });
-        console.log('123error', queryText);
-        return queryText += 'ON CONFLICT (stockSymbol, DATE, TABLE_ID) DO NOTHING;'; // update if there is a duplicate
     }
 
     public postUpload(): void{
-        App.router.post('/upload',
-        [
-            check('stock_symbol').isLength({ min: 1 }),
-            check('data').isLength({min: 1}),
-        ],
-        (req: Request, res: Response) => {
-            // Finds the validation errors in this request and wraps them in an object with handy functions
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(422).json({ errors: errors.array() });
-            }
+        // App.router.post('/upload2',
+        // [
+        //     check('stock_symbol').isLength({ min: 1 }),
+        //     check('data').isLength({min: 1}),
+        // ],
+        // (req: Request, res: Response) => {
+        //     // Finds the validation errors in this request and wraps them in an object with handy functions
+        //     const errors = validationResult(req);
+        //     if (!errors.isEmpty()) {
+        //         return res.status(422).json({ errors: errors.array() });
+        //     }
 
+        //     // insert data to database
+        //     App.db.query(this.uploadQueryText(req), (err, res2) => {
+        //         if(err){
+        //             console.log(err);
+        //             res.json({status: 'ERROR', error: true});
+        //         } else {
+        //             res.json({status: 'SUCCESS', error: false});
+        //         }
+        //     });
+        // });
+
+        const upload = require('jquery-file-upload-middleware');
+        upload.configure({
+            uploadDir: __dirname + '/public/uploads/',
+            uploadUrl: '/uploads',
+        });
+
+        /// Redirect all to home except post
+        App.express.get('/upload', ( req, res ) => {
+            res.redirect('/');
+        });
+
+        App.express.put('/upload', ( req, res ) => {
+            res.redirect('/');
+        });
+
+        App.express.delete('/upload', ( req, res ) => {
+            res.redirect('/');
+        });
+
+        App.express.use('/upload', (req, res, next) => {
+            upload.fileHandler({
+                uploadDir: () => {
+                    return __dirname + '/public/uploads/';
+                },
+                uploadUrl: () => {
+                    return '/uploads';
+                },
+            })(req, res, next);
+        });
+
+        upload.on('end', (fileInfo, req, res) => {
             // insert data to database
-            App.db.query(this.uploadQueryText(req), (err, res2) => {
+            const fileName: string = fileInfo.name;
+            const pathName: string = __dirname + '/public/uploads/' + fileName;
+            this.uploadQueryText(fileName, pathName, (err, queryString) =>{
                 if(err){
-                    console.log(err);
-                    res.json({status: 'ERROR', error: true});
-                } else {
-                    res.json({status: 'SUCCESS', error: false});
+                    // res.json({status: 'ERROR', error: true});
+                } else if(queryString){
+                    App.db.query(queryString, (err2, res2) => {
+                        if(err2){
+                            console.log(err2);
+                            // res.json({status: 'ERROR', error: true});
+                        } else {
+                            // res.json({status: 'SUCCESS', error: false});
+                        }
+                    });
                 }
             });
         });
@@ -146,7 +210,7 @@ class MainController implements IMainController {
     }
 
     private getStdev(res1: QueryResult): number {
-        const close: Number[] = [];
+        const close: any[] = []; // close: nmber[]
         res1.rows.forEach((row: IStock) => {
             close.push(row.close);
         });
